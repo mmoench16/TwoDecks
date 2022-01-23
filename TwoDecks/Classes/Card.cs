7344bfaf04824bef4fe9b571c9d766bfc93d04be
mmoench16 <m.moench16@gmail.com>
2022-01-23 18:20:49 +0100

    Added classes and enums. Also added the data binding to xaml.

---
+++ TwoDecks/Classes/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDecks
+{
+    class Card
+    {
+        public Suit Suit { get; private set; }
+        public Value Value { get; private set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public Card(Suit suit, Value value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+    }
+}
