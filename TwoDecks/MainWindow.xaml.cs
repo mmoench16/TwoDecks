169a3af10cd9c057672a68ece047e1498da88e74
mmoench16 <m.moench16@gmail.com>
2022-01-24 17:45:16 +0100

    Added MoveCard method in xaml.cs

--- TwoDecks/MainWindow.xaml.cs
+++ TwoDecks/MainWindow.xaml.cs
@@ -23,6 +23,79 @@ namespace TwoDecks
         public MainWindow()
         {
             InitializeComponent();
+
+            if (Resources["rightDeck"] is Deck rightDeck)
+            {
+                rightDeck.Clear();
+            }
+        }
+
+        private void MoveCard(bool leftToRight)
+        {
+            if ((Resources["rightDeck"] is Deck rightDeck) && (Resources["leftDeck"] is Deck leftDeck))
+            {
+                if (leftToRight)
+                {
+                    if (leftDeckListBox.SelectedItem is Card cardL)
+                    {
+                        leftDeck.Remove(cardL);
+                        rightDeck.Add(cardL);
+                    } else
+                    {
+                        if (rightDeckListBox.SelectedItem is Card cardR)
+                        {
+                            rightDeck.Remove(cardR);
+                            leftDeck.Add(cardR);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void shuffleLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resetLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clearRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sortRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void leftDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveCard(true);
+            }
+        }
+
+        private void leftDeckListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(true);
+        }
+
+        private void rightDeckListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(false);
+        }
+
+        private void rightDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MoveCard(false);
+            }
         }
     }
 }
