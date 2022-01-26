1822f6c1c29a035c99f460d5c05d8d6e115a9f33
mmoench16 <m.moench16@gmail.com>
2022-01-26 19:41:24 +0100

    Implementing Deck methods

--- TwoDecks/Classes/Deck.cs
+++ TwoDecks/Classes/Deck.cs
@@ -9,6 +9,48 @@ namespace TwoDecks
 {
     class Deck : ObservableCollection<Card>
     {
-        //...
+        private static Random random = new Random();
+
+        public Deck()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Clear();
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 1; value < 14; value++)
+                {
+                    Add(new Card((Suit)suit, (Value)value));
+                }
+            }
+        }
+
+        public Card Deal(int index)
+        {
+            Card card = base[index];
+            RemoveAt(index);
+            return card;
+        }
+
+        public void Shuffle() {
+            List<Card> copy = new List<Card>(this);
+            Clear();
+            while (copy.Count > 0)
+            {
+                int index = random.Next(copy.Count);
+                Card card = copy[index];
+                copy.RemoveAt(index);
+                Add(card);
+            }
+        }
+        public void Sort() {
+            List<Card> sortedCards = new List<Card>(this);
+            sortedCards.Sort(new CardComparerByValue());
+            // Use a foreach loop to call Add for each card in sortedCards
+            throw new NotImplementedException("The Sort method sorts the cards");
+        }
     }
 }
